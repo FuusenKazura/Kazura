f2b82a2feb2caf5928152af596cba247b60839a3
sohei yamaga <me@eliza.link>
2019-12-19 04:16:44 +0900

    modulesの各IOを定義

--- src/main/scala/kazura/modules/ALU.scala
+++ src/main/scala/kazura/modules/ALU.scala
@@ -6,7 +6,7 @@ import kazura.util.Params._
 class ALUIO extends Bundle {
   val alu_op = Input(UInt(ALUOP.NUM_W.W))
   val source = Input(Vec(2, UInt(LEN.W)))
-  val out = Output(UInt(LEN.W))
+  val alu_out = Output(UInt(LEN.W))
 }
 
 class ALU extends Module {
--- src/main/scala/kazura/modules/Memory.scala
+++ src/main/scala/kazura/modules/Memory.scala
@@ -1,5 +1,20 @@
 package kazura.modules
 
-class Memory {
+import chisel3._
+import kazura.util.Params._
 
+class MemoryIO extends Bundle {
+  val alu_out: UInt = Input(UInt(LEN.W))
+  val in: MemoryIn = Input(new MemoryIn)
+  val out: UInt = Output(UInt(LEN.W))
+}
+
+class MemoryIn extends Bundle {
+  val mem_r: Bool = Bool()
+  val mem_w: Bool = Bool()
+  val rd_addr: UInt = UInt(RF.NUM_W.W)
+}
+
+class Memory extends Module {
+  val io = IO(new MemoryIO)
 }
