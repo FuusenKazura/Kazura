d0d85b43a03634017b548f98c15c5b0458093e2c
sohei yamaga <me@eliza.link>
2019-12-19 04:54:12 +0900

    FetchのIOを変更 inst忘れてた……

--- src/main/scala/kazura/modules/Fetch.scala
+++ src/main/scala/kazura/modules/Fetch.scala
@@ -2,6 +2,7 @@ package kazura.modules
 
 import chisel3._
 import chisel3.util.MuxCase
+import kazura.models.Inst
 import kazura.util.Params._
 
 class IFIO extends Bundle {
@@ -16,12 +17,14 @@ class IFIn extends Bundle {
 class IFOut extends Bundle {
   val pc: UInt = UInt(LEN.W)
   val total_cnt: UInt = UInt(LEN.W)
+  val inst: Inst = new Inst
 }
 
-class Fetch extends Module  {
+class Fetch(init: Seq[UInt] = (0 until 256).map(_.U)) extends Module  {
   val io: IFIO = IO(new IFIO)
   val total_cnt: UInt = RegInit(0.U(LEN.W))
   val pc: UInt = RegInit(0.U(LEN.W))
+  val inst_mem = RegInit(VecInit(init))
   total_cnt := total_cnt + 1.U
   pc := MuxCase(pc + 1.U, Seq(
     io.in.is_branch_or_jump_detected -> io.in.alu_out,
@@ -29,5 +32,6 @@ class Fetch extends Module  {
   ))
   io.out.pc := pc
   io.out.total_cnt := total_cnt
+  io.out.inst := inst_mem(pc)
 }
 
