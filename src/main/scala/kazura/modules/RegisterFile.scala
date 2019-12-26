3b8ded2b1279bad637c3cf048222bc47e570933d
sohei yamaga <me@eliza.link>
2019-12-26 18:47:42 +0900

    RegisterFileを定義した

--- src/main/scala/kazura/modules/RegisterFile.scala
+++ src/main/scala/kazura/modules/RegisterFile.scala
@@ -5,9 +5,9 @@ import kazura.util.Params._
 import kazura.models.InstBits
 
 class RFIO extends Bundle {
-  val reg_addr: Vec[UInt] = Vec(RF.READ_PORT, UInt(RF.NUM_W.W))
-  val in = Vec(RF.WRITE_PORT, Input(new RFWrite))
-  val out = Vec(RF.READ_PORT, Output(UInt(LEN.W)))
+  val read_rd_addr: Vec[UInt] = Vec(RF.READ_PORT, Input(UInt(RF.NUM_W.W)))
+  val write: Vec[RFWrite] = Vec(RF.WRITE_PORT, Input(new RFWrite))
+  val out: Vec[UInt] = Vec(RF.READ_PORT, Output(UInt(LEN.W)))
 }
 
 class RFWrite extends Bundle {
@@ -17,5 +17,13 @@ class RFWrite extends Bundle {
 }
 
 class RegisterFile extends Module {
-  val io = IO(new RFIO)
+  val io: RFIO = IO(new RFIO)
+  val rf: Vec[UInt] = Reg(Vec(RF.NUM, UInt(LEN.W)))
+  for (i <- 0 until RF.READ_PORT) {
+    io.out(i) := rf(io.read_rd_addr(i))
+    when (io.write(i).rf_w) {
+      rf(io.write(i).rd_addr) := io.write(i).data
+    }
+  }
+  rf(0) := 0.U
 }
