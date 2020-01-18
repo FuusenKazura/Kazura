a62eecb1455b4bfac0c6e8e58a03cbd668f9f626
Sohei Yamaga <me@eliza.link>
2020-01-18 14:00:50 +0900

    [WIP] ROB

--- src/main/scala/kazura/modules/ROB.scala
+++ src/main/scala/kazura/modules/ROB.scala
@@ -46,22 +46,43 @@ class ROB extends Module {
   buf_init.mem_w := false.B
 
   val buf: Vec[ROBEntry] = RegInit(VecInit(Seq.fill(ROB.BUF_SIZE)(buf_init)))
-  val uncommited_head: UInt = RegInit(0.U(log2Ceil(ROB.BUF_SIZE).W))
-  val unreserved_head: UInt = RegInit(0.U(log2Ceil(ROB.BUF_SIZE).W))
+  val uncommited_head_r: UInt = RegInit(0.U(log2Ceil(ROB.BUF_SIZE).W))
+  val unreserved_head_r: UInt = RegInit(0.U(log2Ceil(ROB.BUF_SIZE).W))
 
+  // graduatesの中ではじめに失敗している命令を抽出
+  // TODO: 複数分岐命令が同時にgraduateした際にバグる可能性が高い
+  //  リングバッファ上のアドレスのみで、古いほうの分岐命令をselectするのは無理なので、なにか仕組みを導入する必要がある
+  val first_mispredicted: Valid[ROBGraduate] = io.graduate.reduceTree(
+    (g1: Valid[ROBGraduate], g2: Valid[ROBGraduate]) => Mux(g1.valid && g1.bits.mispredicted, g1, g2))
+  val mispredicted_addr: Valid[UInt] = Wire(Valid(UInt()))
+  mispredicted_addr.valid :=
+    first_mispredicted.valid && first_mispredicted.bits.mispredicted && buf(first_mispredicted.bits.addr).empty
+  mispredicted_addr.bits := first_mispredicted.bits.addr
+  printf("mispredicted_rob: %d | %d\n", mispredicted_addr.valid, mispredicted_addr.bits)
+
+  val unreserved_head: Valid[UInt] = Wire(Valid(UInt(log2Ceil(ROB.BUF_SIZE).W)))
   // ROBがreserveされた分だけ進める(不可能なら進めない)
-  val next_unreserved_head: UInt = unreserved_head + io.used_num
+  val next_unreserved_head: UInt = unreserved_head_r + io.used_num
   val next_unreserved_head_available: Bool = buf(next_unreserved_head).empty
-  io.unreserved_head.valid := next_unreserved_head_available
-  io.unreserved_head.bits := next_unreserved_head
-  unreserved_head := Mux(next_unreserved_head_available, next_unreserved_head, unreserved_head)
-  io.unreserved_head.bits := unreserved_head
-  io.unreserved_head.valid := buf(unreserved_head).empty
+  printf("next_rob: %d | %d\n", next_unreserved_head_available, next_unreserved_head)
+
+  when (mispredicted_addr.valid) {
+    unreserved_head.valid := mispredicted_addr.valid
+    unreserved_head.bits := mispredicted_addr.bits
+  } .otherwise {
+    unreserved_head.valid := next_unreserved_head_available
+    unreserved_head.bits := next_unreserved_head
+  }
+  printf("rob: %d | %d\n", unreserved_head.valid, unreserved_head.bits)
+
+  unreserved_head_r := Mux(unreserved_head.valid, unreserved_head.bits, unreserved_head_r)
+  io.unreserved_head.valid := buf(unreserved_head_r).empty
+  io.unreserved_head.bits := unreserved_head_r
 
   // このROB Entryをcommitするかどうか
   val committable: Vec[Bool] = Wire(Vec(PARALLEL, Bool()))
   for (i <- committable.indices) {
-    val addr: UInt = uncommited_head + i.U
+    val addr: UInt = uncommited_head_r + i.U
     if (i == 0) {
       committable(i) := buf(addr).committable
     } else {
@@ -69,29 +90,30 @@ class ROB extends Module {
     }
   }
   val committable_cnt: UInt = committable.count(x => x)
-  uncommited_head := uncommited_head + committable_cnt
+  uncommited_head_r := uncommited_head_r + committable_cnt
   printf(s"commmitable_cnt: %d\n", committable_cnt)
 
+  // commit初期値
   for (i <- io.commit.indices) {
     io.commit(i).rf_w := false.B; io.commit(i).rd_addr := 0.U; io.commit(i).data := 0.U
   }
   for (i <- buf.indices) {
-    when(uncommited_head <= i.U && i.U < uncommited_head+PARALLEL.U) {
+    when(uncommited_head_r <= i.U && i.U < uncommited_head_r+PARALLEL.U) { // commit
       buf(i).empty := true.B
-      val l = i.U - uncommited_head
+      val l = i.U - uncommited_head_r
       printf(s"commit(%d).data = %d\n", l, buf(i).alu_out)
       io.commit(l).rf_w := buf(i).reg_w
       io.commit(l).rd_addr := buf(i).rd_addr
       io.commit(l).data := buf(i).alu_out
-    } .elsewhen(unreserved_head <= i.U && i.U < unreserved_head+io.used_num) {
+    } .elsewhen(unreserved_head.valid && unreserved_head.bits <= i.U && i.U < unreserved_head.bits+io.used_num) { // dispatch
       buf(i).empty := false.B
     } .otherwise {
+      // EX, MAから出てくるデータを取り込む、それらのデータはDispatch, Commitの対象になることはないので問題なし
+
       val graduate = io.graduate.reduceTree((g1: Valid[ROBGraduate], g2: Valid[ROBGraduate]) =>
         Mux(g1.valid && g1.bits.addr === i.U, g1, g2))
 
-      mispredict_restore_rob_addr_v(i) := false.B
       when (graduate.valid && i.U === graduate.bits.addr) {
-        mispredict_restore_rob_addr_v(i) := true.B
         buf(i).committable := true.B
         buf(i).is_halt := graduate.bits.is_halt
         buf(i).reg_w := graduate.bits.reg_w
--- src/test/scala/modules/ROBUnitTester.scala
+++ src/test/scala/modules/ROBUnitTester.scala
@@ -8,7 +8,7 @@ class ROBUnitTester(m: ROB) extends PeekPokeTester(m) {
   expect(m.io.unreserved_head.valid, true)
   expect(m.io.unreserved_head.bits, 0)
   poke(m.io.used_num, 4)
-  for (i <- m.io.graduate.indices) poke(m.io.graduate(i).valid, false)
+  for (i <- m.io.graduate.indices) { poke(m.io.graduate(i).valid, false); poke(m.io.graduate(i).bits.mispredicted, false) }
   step(1) // ------------------------------
 
   for (i <- m.io.commit.indices) expect(m.io.commit(i).rf_w, false)
@@ -16,26 +16,26 @@ class ROBUnitTester(m: ROB) extends PeekPokeTester(m) {
   expect(m.io.unreserved_head.bits, 4)
   poke(m.io.used_num, 0)
   // for (i <- m.io.graduate.indices) poke(m.io.graduate(i).valid, false)
-  poke(m.io.graduate(0).valid, false)
-  poke(m.io.graduate(1).valid, false)
-  poke(m.io.graduate(2).valid, true)
+  poke(m.io.graduate(0).valid, false); poke(m.io.graduate(0).bits.mispredicted, false)
+  poke(m.io.graduate(1).valid, false); poke(m.io.graduate(1).bits.mispredicted, false)
+  poke(m.io.graduate(2).valid, true);  poke(m.io.graduate(2).bits.mispredicted, false)
   poke(m.io.graduate(2).bits.addr, 1)
   poke(m.io.graduate(2).bits.rd_addr, 1)
   poke(m.io.graduate(2).bits.alu_out, 10)
-  poke(m.io.graduate(3).valid, false)
+  poke(m.io.graduate(3).valid, false); poke(m.io.graduate(3).bits.mispredicted, false)
   step(1) // ------------------------------
 
   for (i <- m.io.commit.indices) expect(m.io.commit(i).rf_w, false)
   expect(m.io.unreserved_head.valid, true)
   expect(m.io.unreserved_head.bits, 4)
   poke(m.io.used_num, 2)
-  poke(m.io.graduate(0).valid, true)
+  poke(m.io.graduate(0).valid, true);  poke(m.io.graduate(0).bits.mispredicted, false)
   poke(m.io.graduate(0).bits.addr, 0)
   poke(m.io.graduate(0).bits.rd_addr, 2)
   poke(m.io.graduate(0).bits.alu_out, 20)
-  poke(m.io.graduate(1).valid, false)
-  poke(m.io.graduate(2).valid, false)
-  poke(m.io.graduate(3).valid, false)
+  poke(m.io.graduate(1).valid, false); poke(m.io.graduate(1).bits.mispredicted, false)
+  poke(m.io.graduate(2).valid, false); poke(m.io.graduate(2).bits.mispredicted, false)
+  poke(m.io.graduate(3).valid, false); poke(m.io.graduate(3).bits.mispredicted, false)
   step(1) // ------------------------------
 
   expect(m.io.commit(0).rf_w, true)
@@ -50,12 +50,12 @@ class ROBUnitTester(m: ROB) extends PeekPokeTester(m) {
   expect(m.io.unreserved_head.valid, true)
   expect(m.io.unreserved_head.bits, 6)
   poke(m.io.used_num, 0)
-  for (i <- m.io.graduate.indices) poke(m.io.graduate(i).valid, false)
+  for (i <- m.io.graduate.indices) { poke(m.io.graduate(i).valid, false); poke(m.io.graduate(i).bits.mispredicted, false) }
   step(1) // ------------------------------
 
   for (i <- m.io.commit.indices) expect(m.io.commit(i).rf_w, false)
   expect(m.io.unreserved_head.valid, true)
   expect(m.io.unreserved_head.bits, 6)
   poke(m.io.used_num, 4)
-  for (i <- m.io.graduate.indices) poke(m.io.graduate(i).valid, false)
+  for (i <- m.io.graduate.indices) { poke(m.io.graduate(i).valid, false); poke(m.io.graduate(i).bits.mispredicted, false) }
 }
